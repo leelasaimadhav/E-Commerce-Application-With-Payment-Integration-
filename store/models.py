45f04154825352e356cf9b17bb00e86b833d1a6f
Madhav Manne <leelasaimadhavWgmail.com>
2018-06-15 14:57:12 -0400

    Added Book Model,Book Admin Class, changed store Template

--- store/models.py
+++ store/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
-# Create your models here.
+from django.utils import timezone
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    description =models.TextField()
+    publish_date = models.DateField(default=timezone.now)
+    price = models.DecimalField(max_digits=8,decimal_places=2)
+    stock = models.IntegerField(default=0)
